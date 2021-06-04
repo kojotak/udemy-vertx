565232dcdabea5478698135e0d47c6c260ffc96e
Tomáš Beneda <tomas.beneda@equabank.cz>
2021-06-04 15:06:46 +0200

    event bus - publish / subscribe

---
+++ src/main/java/cz/kojotak/udemy/vertx/eventbus/PublishSubscribeExample.java
@@ -0,0 +1,49 @@
+package cz.kojotak.udemy.vertx.eventbus;
+
+import java.time.Duration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+
+public class PublishSubscribeExample {
+
+	public static void main(String[] args) {
+		var vertx = Vertx.vertx();
+		vertx.deployVerticle(new Publish());
+		vertx.deployVerticle(new Subscriber1());
+		vertx.deployVerticle(new Subscriber2());
+	}
+	
+	static class Publish extends AbstractVerticle {
+
+		@Override
+		public void start(Promise<Void> startPromise) throws Exception {
+			startPromise.complete();
+			vertx.setPeriodic(Duration.ofSeconds(1).toMillis(), id->{
+				vertx.eventBus().publish(Publish.class.getName(), "msg for everyone");
+			});
+		}
+		
+	}
+	
+	public static class Subscriber1 extends Subscriber{}
+	public static class Subscriber2 extends Subscriber{}
+	
+	abstract static class Subscriber extends AbstractVerticle {
+		
+		private final Logger LOG = LoggerFactory.getLogger(getClass());
+	
+		@Override
+		public void start(Promise<Void> startPromise) throws Exception {
+			vertx.eventBus().<String>consumer(Publish.class.getName(), msg->{
+				LOG.debug("received {}", msg.body());
+			});
+			startPromise.complete();
+		}
+		
+	}
+}
