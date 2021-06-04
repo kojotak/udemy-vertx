24daae4e20eacd10c9a21daca656ac14550c824b
Tomáš Beneda <tomas.beneda@equabank.cz>
2021-06-04 14:07:47 +0200

    blocking event loop

---
+++ src/main/java/cz/kojotak/udemy/vertx/eventloop/EventLoopExample.java
@@ -0,0 +1,40 @@
+package cz.kojotak.udemy.vertx.eventloop;
+
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import cz.kojotak.udemy.vertx.verticles.MainVerticle;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+
+// rule: never block an event loop
+public class EventLoopExample extends AbstractVerticle {
+
+	private static final Logger LOG = LoggerFactory.getLogger(EventLoopExample.class);
+
+	public static void main(String[] args) {
+		Vertx vertx = Vertx.vertx(
+				new VertxOptions()
+					.setMaxEventLoopExecuteTime(500)
+					.setMaxEventLoopExecuteTimeUnit(TimeUnit.MILLISECONDS)
+					.setBlockedThreadCheckInterval(1)
+					.setBlockedThreadCheckIntervalUnit(TimeUnit.SECONDS)
+					.setEventLoopPoolSize(4)
+				);
+		vertx.deployVerticle(EventLoopExample.class.getName(),
+				new DeploymentOptions().setInstances(4));
+	}
+
+	@Override
+	public void start(Promise<Void> startPromise) throws Exception {
+		LOG.debug("start {}", getClass().getName());
+		startPromise.complete();
+		Thread.sleep(5000);
+	}
+	
+}
