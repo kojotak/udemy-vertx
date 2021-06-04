0c6f350009f51dc1837b4190a38adac7759c684d
Tomáš Beneda <tomas.beneda@equabank.cz>
2021-06-04 14:34:07 +0200

    event bus

---
+++ src/main/java/cz/kojotak/udemy/vertx/eventbus/RequestResponseExample.java
@@ -0,0 +1,51 @@
+package cz.kojotak.udemy.vertx.eventbus;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+
+public class RequestResponseExample {
+
+	public static void main(String[] args) {
+		var vertx = Vertx.vertx();
+		vertx.deployVerticle(new RequestVerticle());
+		vertx.deployVerticle(new ResponseVerticle());
+	}
+	
+	static class RequestVerticle extends AbstractVerticle {
+
+		static final String ADDRESS = "my.request.address";
+		private static final Logger LOG = LoggerFactory.getLogger(RequestVerticle.class);
+
+		@Override
+		public void start(Promise<Void> startPromise) throws Exception {
+			startPromise.complete();
+			var eventBus = vertx.eventBus();
+			var msg = "Hello world";
+			LOG.debug("sending {}", msg);
+			eventBus.request(ADDRESS, //advice: use id of the sender
+					msg, //what to send
+					reply -> {
+						LOG.debug("Response {}", reply.result().body());
+					});
+		}
+		
+	}
+	
+	static class ResponseVerticle extends AbstractVerticle {
+		
+		private static final Logger LOG = LoggerFactory.getLogger(ResponseVerticle.class);
+		
+		@Override
+		public void start(Promise<Void> startPromise) throws Exception {
+			startPromise.complete();
+			vertx.eventBus().consumer(RequestVerticle.ADDRESS, msg->{
+				LOG.debug("received {}", msg.body());
+				msg.reply("msg received, thanks");
+			});
+		}
+	}
+}
