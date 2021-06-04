b9a71a57e5859505081057e96eceb9d28689895c
Tomáš Beneda <tomas.beneda@equabank.cz>
2021-06-04 13:29:09 +0200

    starting multiple instances

--- src/main/java/cz/kojotak/udemy/vertx/verticles/MainVerticle.java
+++ src/main/java/cz/kojotak/udemy/vertx/verticles/MainVerticle.java
@@ -1,6 +1,7 @@
 package cz.kojotak.udemy.vertx.verticles;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 
@@ -16,6 +17,8 @@ public class MainVerticle extends AbstractVerticle {
 		System.out.println("start " + getClass().getName());
 		vertx.deployVerticle(new VerticleA());
 		vertx.deployVerticle(new VerticleB());
+		vertx.deployVerticle(VerticleN.class.getName(),
+				new DeploymentOptions().setInstances(4)); //let vertx create all instances
 		promise.complete();
 	}
 
---
+++ src/main/java/cz/kojotak/udemy/vertx/verticles/VerticleN.java
@@ -0,0 +1,14 @@
+package cz.kojotak.udemy.vertx.verticles;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+
+public class VerticleN extends AbstractVerticle {
+
+	@Override
+	public void start(final Promise<Void> promise) throws Exception {
+		System.out.println("start " + getClass().getName() + " in " + Thread.currentThread().getName());
+		promise.complete();
+	}
+
+}
