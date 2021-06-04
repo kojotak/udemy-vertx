a559fe578606646e2524297c1a12cf88e4644d4d
Tomáš Beneda <tomas.beneda@equabank.cz>
2021-06-04 12:45:55 +0200

    first app

--- src/main/java/cz/kojotak/udemy/vertx/starter/MainVerticle.java
+++ src/main/java/cz/kojotak/udemy/vertx/starter/MainVerticle.java
@@ -5,7 +5,12 @@ import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 
 public class MainVerticle extends AbstractVerticle {
-	
+
+  public static void main(String ... args) {
+	  var vertx = Vertx.vertx();
+	  vertx.deployVerticle(new MainVerticle());
+  }
+ 
   public void start(Promise<Void> startPromise) throws Exception {
     vertx.createHttpServer().requestHandler(req -> {
       req.response()
